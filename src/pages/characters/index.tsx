4f12cf216fde267ea3f1c5f423947407ca31fb52
Jadna <jadnajesus7@gmail.com>
2025-03-26 20:13:03 -0300

    feat: add paginacao

--- src/pages/characters/index.tsx
+++ src/pages/characters/index.tsx
@@ -2,11 +2,13 @@ import * as S from "./styles";
 import { PATH } from "../../routes/path";
 import { useEffect, useState } from "react";
 import { Card } from "../../components/card";
-import { StartWarsService } from "../../service/http/star.wars.service";
 import { useNavigate } from "react-router-dom";
+import { StartWarsService } from "../../service/http/star.wars.service";
+import { Loading } from "../../components/loading";
+import { Pagination } from "../../components/pagination";
 
 export type Character = {
-  mass: string;
+  id: string;
   name: string;
   height: string;
   birth_year: string;
@@ -14,14 +16,18 @@ export type Character = {
 };
 
 export const Characters = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
+  const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(true);
+  const [totalPages, setTotalPages] = useState(0);
   const [characters, setCharacters] = useState<Character[]>([]);
 
-  const fetchCharacters = async () => {
+  const fetchCharacters = async (page: number) => {
+    setLoading(true);
     try {
-      const response = await StartWarsService.getCharacters();
+      const response = await StartWarsService.getCharacters({ page });
       setCharacters(response?.data.results);
+      setTotalPages(Math.ceil(response?.data.count / 10));
       setLoading(false);
     } catch (error) {
       console.error(error);
@@ -31,31 +37,40 @@ export const Characters = () => {
   };
 
   useEffect(() => {
-    fetchCharacters();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+    fetchCharacters(page);
+  }, [page]);
 
   return (
-    <S.Grid>
-      {characters.map((item) => (
-        <Card
-          id={item.mass}
-          key={item.name}
-          title={item.name}
-          details={[
-            { label: "Height", value: item.height },
-            { label: "BirthYear", value: item.birth_year },
-            {
-              label: "Homeworld",
-              value: item.url,
-              link: `${PATH.DETAILS}/${item.url.split("/")[5]}`,
-            },
-          ]}
-        />
-      ))}
-    </S.Grid>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <S.Grid>
+            {characters?.map((item) => (
+              <Card
+                id={item.id}
+                key={item.name}
+                title={item.name}
+                details={[
+                  { label: "Height", value: item.height },
+                  { label: "BirthYear", value: item.birth_year },
+                  {
+                    label: "Homeworld",
+                    value: item.url,
+                    link: `${PATH.DETAILS}/${item.url.split("/")[5]}`,
+                  },
+                ]}
+              />
+            ))}
+          </S.Grid>
+          <Pagination
+            currentPage={page}
+            totalPages={totalPages}
+            onPageChange={(page) => setPage(page)}
+          />
+        </>
+      )}
+    </>
   );
 };
