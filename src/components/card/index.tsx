b9284cc75ec17cb97025f00dccdb2f9901170dd8
Jadna <jadnajesus7@gmail.com>
2025-03-26 16:49:06 -0300

    feat: component de card

---
+++ src/components/card/index.tsx
@@ -0,0 +1,45 @@
+import { useNavigate } from "react-router-dom";
+import * as S from "./styles";
+
+export type CardProps = {
+  id: string;
+  title: string;
+  details: {
+    label: string;
+    value?: string;
+    link?: string;
+  }[];
+};
+
+export const Card = ({ id, title, details }: CardProps) => {
+  const navigate = useNavigate();
+
+  const handleNavigation = (link: string | undefined) => {
+    if (link) {
+      navigate(link);
+    }
+  };
+  return (
+    <>
+      <S.CardContainer>
+        <S.CardContent>
+          <S.CardTitle>{title}</S.CardTitle>
+          <S.CardList>
+            {details.map((detail) => (
+              <S.CardListItem key={id}>
+                <strong>{detail.label}:</strong>{" "}
+                {detail.link ? (
+                  <span onClick={() => handleNavigation(detail.link)}>
+                    {detail.value}
+                  </span>
+                ) : (
+                  detail.value
+                )}
+              </S.CardListItem>
+            ))}
+          </S.CardList>
+        </S.CardContent>
+      </S.CardContainer>
+    </>
+  );
+};
---
+++ src/components/card/styles.ts
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import { DEVICES } from "../../styles/theme/devices";
+import { THEME_COLORS } from "../../styles/theme/colors";
+
+export const CardContainer = styled.div`
+  display: flex;
+  padding: 20px;
+  margin: 20px;
+  align-items: center;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  flex-direction: column;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+  @media (max-width: ${DEVICES.MOBILE}) {
+    width: 100%;
+    margin: 10px;
+  }
+`;
+
+export const Logo = styled.h1`
+  width: 50px;
+  height: 50px;
+  margin-bottom: 10px;
+`;
+
+export const CardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const CardTitle = styled.h2`
+  font-size: 18px;
+  font-weight: 700;
+  color: ${THEME_COLORS.AZUL_ESCURO};
+`;
+
+export const CardList = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+export const CardListItem = styled.li`
+  margin-bottom: 8px;
+  font-size: 1rem;
+  color: ${THEME_COLORS.PRETO};
+
+  p {
+    color: ${THEME_COLORS.AZUL_ESCURO};
+    font-size: 16px;
+    font-weight: 400;
+  }
+
+  span {
+    color: blue;
+    cursor: pointer;
+    text-decoration: underline;
+
+    &:hover {
+      color: ${THEME_COLORS.PRETO};
+    }
+  }
+`;
