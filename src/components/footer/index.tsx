13a84c9705160e07d7b107af82c61c505401d886
Jadna <jadnajesus7@gmail.com>
2025-03-26 16:49:28 -0300

    feat: footer da aplicacao

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,9 @@
+import * as S from "./styles";
+import logo from "../../assets/logo.svg";
+export const Footer = () => {
+  return (
+    <S.Footer>
+      <img src={logo} alt="Logo" />
+    </S.Footer>
+  );
+};
