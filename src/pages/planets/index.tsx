6ee27a191b7403fe4279118bc835703b5c082211
Jadna <jadnajesus7@gmail.com>
2025-03-27 17:22:38 -0300

    refactor: remove a logica do UI planets

--- src/pages/planets/index.tsx
+++ src/pages/planets/index.tsx
@@ -1,39 +1,13 @@
-import { useEffect, useState } from "react";
-import { StartWarsService } from "../../service/http/star.wars.service";
-import { Card } from "../../components/card";
 import * as S from "./styles";
+import { Card } from "../../components/card";
+import { Search } from "../../components/search";
 import { Loading } from "../../components/loading";
+import { usePlanets } from "../../hooks/usePlanets";
 import { Pagination } from "../../components/pagination";
 
-export type Planets = {
-  id: string;
-  name: string;
-  climate: string;
-  residents: string[];
-  orbital_period: string;
-};
 export const Planets = () => {
-  const [page, setPage] = useState(1);
-  const [loading, setLoading] = useState(true);
-  const [totalPages, setTotalPages] = useState(0);
-  const [planets, setPlanets] = useState<Planets[]>([]);
-
-  const fetchPlanets = async (page: number) => {
-    try {
-      const response = await StartWarsService.getPlanets({ page });
-      setPlanets(response?.data.results);
-      setTotalPages(Math.ceil(response?.data.count / 10));
-      setLoading(false);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchPlanets(page);
-  }, [page]);
+  const { loading, planets, totalPages, page, setPage, search, setSearch } =
+    usePlanets();
 
   return (
     <>
@@ -41,6 +15,12 @@ export const Planets = () => {
         <Loading />
       ) : (
         <>
+          <Search
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search planets..."
+            onClick={() => setPage(1)}
+          />
           <S.GridPlanet>
             {planets?.map((item) => (
               <Card
@@ -51,9 +31,9 @@ export const Planets = () => {
                   { label: "Orbital Rotation", value: item.orbital_period },
                   { label: "Climate", value: item.climate },
                   {
-                    label: "Homeworld",
-                    value: "iAOAOAO",
-                    link: "AOAOAO",
+                    label: "Residents",
+                    value: item.residents.join(", "),
+                    link: `/details/${item.id}`,
                   },
                 ]}
               />
