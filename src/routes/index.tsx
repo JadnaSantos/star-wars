5d3debc54dcb896363c8d3e76b2217fd23488cb1
Jadna <jadnajesus7@gmail.com>
2025-03-27 18:41:22 -0300

    feat: adiciona nova rota favoritos

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { PATH } from "./path";
 import { Layout } from "../layout";
 import { Planets } from "../pages/planets";
+import { DetailPage } from "../pages/details";
+import { Favorites } from "../pages/favorites";
 import { Characters } from "../pages/characters";
 import { createBrowserRouter } from "react-router-dom";
-import { Details } from "../pages/details";
 
 export const routes = createBrowserRouter([
   {
@@ -26,7 +27,15 @@ export const routes = createBrowserRouter([
     path: PATH.DETAILS,
     element: (
       <Layout>
-        <Details />
+        <DetailPage />
+      </Layout>
+    ),
+  },
+  {
+    path: PATH.FAVORITES,
+    element: (
+      <Layout>
+        <Favorites />
       </Layout>
     ),
   },
--- src/routes/path.ts
+++ src/routes/path.ts
@@ -1,6 +1,6 @@
 export const PATH = {
   CHARACTERS: "/",
   PLANETS: "/planets",
+  DETAILS: "/details",
   FAVORITES: "/favorites",
-  DETAILS: "/planets/:id",
 };
