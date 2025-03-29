68cf54df2a0d5853ab2f402fe72586b88af91944
Jadna <jadnajesus7@gmail.com>
2025-03-28 21:30:16 -0300

    refactor: component card ajustando a logica para renderizar a listagem de urls

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,26 +1,29 @@
+import { PATH } from "../../routes/path";
 import * as S from "./styles";
 import { useNavigate } from "react-router-dom";
-import { FaStar, FaRegStar } from "react-icons/fa";
-import { useFavorites } from "../../hooks/useFavorite";
 
 export type CardProps = {
-  id: string;
+  id?: string;
   title: string;
   details: {
     label: string;
-    value?: string | React.ReactNode;
-    link?: string;
+    value?: string;
+    link?: string | Array<string>;
   }[];
 };
 
 export const Card = ({ id, title, details }: CardProps) => {
   const navigate = useNavigate();
 
-  const handleNavigation = (link: string | undefined) => {
-    if (link) {
-      navigate(link);
+  const handleNavigation = (link: string) => {
+    const match = link.match(/https:\/\/swapi.dev\/api\/(\w+)\/(\d+)\//);
+    if (match) {
+      const [, type, id] = match;
+
+      navigate(`/details/${type}/${id}`);
     }
   };
+
   return (
     <>
       <S.CardContainer>
@@ -29,14 +32,27 @@ export const Card = ({ id, title, details }: CardProps) => {
           <S.CardList key={id}>
             {details.map((detail) => (
               <S.CardListItem key={id}>
-                <p>{detail.label}:</p>{" "}
-                {detail.link ? (
-                  <span onClick={() => handleNavigation(detail.link)}>
-                    {detail.value}
-                  </span>
-                ) : (
-                  detail.value
-                )}
+                <S.CardListItens>
+                  <p>{detail.label}:</p>
+                  <p>{detail.value}</p>
+                </S.CardListItens>
+                {Array.isArray(detail.link) ? (
+                  <>
+                    {detail.link.map((multiploLink) => (
+                      <S.CardLink
+                        onClick={() => handleNavigation(multiploLink)}
+                      >
+                        {multiploLink}
+                      </S.CardLink>
+                    ))}
+                  </>
+                ) : typeof detail.link === "string" ? (
+                  <S.CardLink
+                    onClick={() => handleNavigation(detail.link as string)}
+                  >
+                    {detail.link}
+                  </S.CardLink>
+                ) : null}
               </S.CardListItem>
             ))}
           </S.CardList>
